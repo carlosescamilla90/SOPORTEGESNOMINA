95566dab996f09ae0f0097f4e07144765810e607
carlosescamilla90 <carlos.escamillarodriguez@unicolombo.edu.co>
2024-09-03 13:14:39 -0500

    Actualizacion1

---
+++ clases/Employee.java
@@ -0,0 +1,13 @@
+package clases;
+
+import java.util.List;
+
+public class Employee {
+    private String dni;
+    private String name;
+    private String lastName;
+    private String telephoneNumber;
+    private String address;
+    private String accountNumberBank;
+    private List<Contrac> listContrato;
+}
